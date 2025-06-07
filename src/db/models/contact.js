fad4cb6b3f0c34cf2b6764ee253ce6d8cf2100d8
Yurii <evseevyrij90@gmail.com>
2025-06-07 13:58:17 +0200

    finisch-branch2

---
+++ src/db/models/contact.js
@@ -0,0 +1,22 @@
+import { model, Schema } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String },
+    isFavourite: { type: Boolean, default: false },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactsCollection = model('contacts', contactSchema);
