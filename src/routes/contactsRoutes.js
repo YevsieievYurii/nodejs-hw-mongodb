a0bd62b2356d5facca62ab009adb7923273271cc
Yurii <evseevyrij90@gmail.com>
2025-06-09 19:35:24 +0200

    change done

--- src/routes/contactsRoutes.js
+++ src/routes/contactsRoutes.js
@@ -6,7 +6,7 @@ import {
 
 const router = express.Router();
 
-router.get('/:contacts', getContactsController);
+router.get('/', getContactsController);
 router.get('/:contactId', getContactByIdController);
 
 export default router;
