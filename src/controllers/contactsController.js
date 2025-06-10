37861584d9cb72a32d8f20683938dc25b7cd7920
Yurii <evseevyrij90@gmail.com>
2025-06-10 19:41:13 +0200

    change students

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -2,11 +2,11 @@ import { getAllContacts, getContactById } from '../services/contacts.js';
 
 export const getContactsController = async (req, res) => {
   try {
-    const students = await getAllContacts();
+    const contacts = await getAllContacts();
     res.status(200).json({
       status: 200,
       message: 'Successfully found contacts!',
-      data: students,
+      data: contacts,
     });
   } catch (error) {
     res.status(500).json({ message: 'Server error', error: error.message });
