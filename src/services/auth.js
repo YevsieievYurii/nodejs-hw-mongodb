c3ce4f9b4aea17a28d704b9897694e23086281d4
Yurii <evseevyrij90@gmail.com>
2025-07-13 15:21:28 +0200

    finally

--- src/services/auth.js
+++ src/services/auth.js
@@ -113,7 +113,7 @@ export const requestResetToken = async (email) => {
 
   const resetPasswordTemplatePath = path.join(
     TEMPLATES_DIR,
-    'reset-password-email.html',
+    'reset-password.html',
   );
 
   let html;
