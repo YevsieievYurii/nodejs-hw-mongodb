3c663ff7cb502965870938d1d01b8996240707b6
Yurii <evseevyrij90@gmail.com>
2025-07-16 17:26:34 +0200

    Добавил маршрут /api-docs для Swagger UI

--- src/server.js
+++ src/server.js
@@ -3,12 +3,17 @@ import cors from 'cors';
 import pino from 'pino-http';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
-
 import contactsRouter from './routes/contactsRoutes.js';
 import authRouter from './routes/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { getEnvVar } from './utils/getEnvVar.js';
+
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+const swaggerDocument = JSON.parse(
+  fs.readFileSync('./docs/swagger.json', 'utf8'),
+);
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -22,6 +27,8 @@ export const startServer = () => {
 
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
